32da754800e21e1123e29de62ef8f4396385004c
james7000 <engg.james@gmail.com>
2024-04-11 12:59:34 +0530

    commit with added text in sidebar

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from langchain_google_genai import GoogleGenerativeAI
+st.title('🦜🔗 LaGoSt App : Built with Langchain Google Gemini Streamlit')
+st.sidebar.title("LaGoSt App")
+openai_api_key = st.sidebar.text_input('Input Your Google Studio Gemini API Key. If you do not have you can create for free\
+                                       from here https://aistudio.google.com/app/apikey')
+st.sidebar.divider()  # 👈 Draws a horizontal rule
+
+st.sidebar.markdown("👋 Hi, Thank you for checking my app. My name is James. Connect with me if you are looking for Generative AI developer.\
+                 My Tech stack is : Langchain for LLM Framework, Google Vertex or OpenAI for LLM, StreamLit for frontend. https://www.linkedin.com/in/mascarenhasjames")
+
+def generate_response(input_text):
+  llm = GoogleGenerativeAI(model="gemini-1.0-pro-latest", google_api_key=openai_api_key)
+  st.info(llm(input_text))
+
+with st.form('my_form'):
+  text = st.text_area('Enter text:', 'I am a software developer. Roast Me?')
+  submitted = st.form_submit_button('Submit')
+  if not openai_api_key:
+    st.warning('Please enter your Gemini API Key OR create for free  https://aistudio.google.com/app/apikey', icon='⚠')
+  if submitted and openai_api_key:
+    generate_response(text)
---
+++ helpers/gemini.py
@@ -0,0 +1,49 @@
+from langchain.prompts import PromptTemplate
+from langchain.chains.llm import LLMChain
+from langchain_core.language_models.llms import LLM
+from langchain_google_genai import GoogleGenerativeAI
+from langchain.chains.sequential import SequentialChain
+
+
+
+def googleapi(apikey):
+    llm1 = GoogleGenerativeAI(model="gemini-1.0-pro-latest", google_api_key=apikey)
+    return llm1
+
+def restNameGen(cusine):
+     # Create the language model with your API key
+    llm1 = GoogleGenerativeAI(model="gemini-1.0-pro-latest", google_api_key="")
+
+    # Template for suggesting a restaurant name
+    Template1 = PromptTemplate(
+        input_variables=["cuisine"],
+        template="I want to open a restaurant for {cuisine} cuisine. Suggest one fancy name for it."
+    )
+
+    # Chain for generating a restaurant name
+    chain1 = LLMChain(llm=llm1, prompt=Template1, output_key="restaurant_name")
+
+    # Template for suggesting menu items
+    Template2 = PromptTemplate(
+        input_variables=["restaurant_name"],
+        template="Suggest menu items for {restaurant_name}. Return them as comma-separated values."
+    )
+
+    # Chain for generating menu items
+    chain2 = LLMChain(llm=llm1, prompt=Template2, output_key="menu_items")  # Use "menu_items" for clarity
+
+    # Sequential chain to execute both prompts in order
+    ssschain = SequentialChain(
+        chains=[chain1, chain2],
+        input_variables=["cuisine"],
+        output_variables=["restaurant_name", "menu_items"]  # Use "menu_items" for clarity
+    )
+
+    # Execute the chain with input
+    response = ssschain({'cuisine': cusine})
+    # print(response)  # Print the complete response
+
+    return response
+
+if __name__=="__main__":
+    print(restNameGen("indian"))
